2878205868894368c6b7b72326c8784364618fab
Quentin Dreyer <quentin.dreyer@gmail.com>
2014-09-19 13:20:36 +0200

    Logging System, API Lib WIP

--- worker.js
+++ worker.js
@@ -1,35 +1,72 @@
+
+// ======================================================================================================================================================
+// NPM Modules
+// ======================================================================================================================================================
+
+var _ = require("underscore");
 var fs = require("fs");
 var gpio = require("gpio");
+var crypto = require("crypto");
+var promise = require("promise");
+var domain = require("domain").create();
 
+// ======================================================================================================================================================
+// Libs
+// ======================================================================================================================================================
+
+var init = require("./lib/init");
+var api = require("./lib/api");
 var db = require("./lib/db");
 var conf = require("./lib/config");
 var time = require("./lib/time");
+var log = require("./lib/log");
 
-db.events.on('ready', function() {
-	console.log("Starting Worker...");
+// ======================================================================================================================================================
+// Setup Worker
+// ======================================================================================================================================================
 
-	if (process.argv && process.argv.length > 2)
-	{
-		var rfid = process.argv[2];
-		db.presence.create();
-	}
-	console.log(process.argv);
-	return;
-
-	// Autorecalling Function
-    (function forever() {
-        synchronize(function(err) {
-            if (err) console.error(err.message);
-            setTimeout(forever, time.minute);
-        });
-    })();
+domain.on("error", function(err) {
+	log.critical(err);
+}).run(function() {
+	db.init().done(main);
 });
 
-function synchronize(_callback) {
-	var error = new Error();
-	console.log("Synchronizing");
+// ======================================================================================================================================================
+// Worker Functions
+// ======================================================================================================================================================
 
-	db.presence.read("synchronized=0")
+function main() {
+	log.info("Starting Worker...");
 
-	_callback(error);
-}
+	// Simulate Badging
+	if (process.argv && process.argv.length === 3 && process.argv[2] == "badge")
+	{
+		var identifier = "'" + crypto.randomBytes(10).toString("base64") + "'";
+		var datetime = "'" + (new Date).toISOString() + "'";
+		db.presence.create({identifier: identifier, datetime: datetime});
+	}
+
+	// Autorecalling Function
+	(function forever() {
+		sync(function(err) {
+			if (err) log.error(err.message);
+			setTimeout(forever, time.minute);
+		});
+	})();
+};
+
+function sync(_callback) {
+	log.info("Synchronizing");
+
+	db.presence.read("synchronized IS NULL").then(function(rows) {
+
+		var promises = [];
+		for (var i in rows) promises.push(api.presence.post(rows[i]));
+
+		promise.all(promises).then(function(res) {
+			log.warn(res);
+
+			_callback();
+		});
+	});
+};
